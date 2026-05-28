5abceb06732c9c86ec54638dccdcc52967aa5909
hello <hello@users-Mac-mini.local>
2026-05-28 23:55:44 +0300

    fix size issue

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+    <div className="min-h-screen min-w-full bg-white text-black dark:bg-gray-900 dark:text-white">
       <NavBar />
       <main className="container mx-auto px-4 py-8">
         <Outlet />
